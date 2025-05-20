4af5f055751c5f6f53b08bf251a52b9be165e141
Asijit Manna <asijit@browserstack.com>
2025-05-20 12:02:50 +0530

    update tests

--- tests/frontend/test_login.py
+++ tests/frontend/test_login.py
@@ -4,3 +4,6 @@ import pytest
 @pytest.mark.priority("high")
 def test_login():
     assert True  # Simulating a passing test
+
+def test_login_failure():
+    assert False  # Simulating a failing test
